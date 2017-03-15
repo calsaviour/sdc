d2e6d75e63af494d0c378de6ab933f619c0f4abe
calsaviour <calsaviour@gmail.com>
2017-03-15 15:22:02 +0000

    Add drouput

--- TermOne/CarND-Behavioral-Cloning-P3/nvidia.py
+++ TermOne/CarND-Behavioral-Cloning-P3/nvidia.py
@@ -13,7 +13,7 @@ measurements = []
 for line in lines:
   for i in range(3):
     source_path = line[i]
-    filename = source_path.split('\\')[-1]
+    filename = source_path.split('/')[-1]
     current_path = '../data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
@@ -32,7 +32,7 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, SpatialDropout2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -43,19 +43,26 @@ model.add(Cropping2D(cropping=((70,25), (0,0))))
 
 
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(SpatialDropout2D(0.2))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(SpatialDropout2D(0.2))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(SpatialDropout2D(0.2))
 model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(SpatialDropout2D(0.2))
 model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(SpatialDropout2D(0.2))
 
 model.add(Flatten())
+model.add(Dropout(0.5))
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=50)
 
 model.save('nvidia.h5')
 
